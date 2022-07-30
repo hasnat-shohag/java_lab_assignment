5e20a3bccba891a96a7ba85397b3696ab4afd52b
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-07-31 01:43:25 +0600

    future investment value calculation

---
+++ Assignment 1/_4_future_investment_value.java
@@ -0,0 +1,25 @@
+// Write a program that reads in investment amount, annual interest rate, and numbers of years, and displays the future investment value using the following formula: 
+// FutureInvestmentValue = investmentAmount * (1+monthlyInterestRate)^numbeOfYears*12
+// For example, if you enter amount 1000, annual interest rate 3.25%, and the numbers of years 1, the future investment value if 1032.98
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class _4_future_investment_value {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter investment amount: ");
+		double amount = sc.nextDouble();
+
+		System.out.print("Enter annual interest rate in percentage: ");
+		double monthlyInterestRate = sc.nextDouble();
+		monthlyInterestRate /= (12*100);
+
+		System.out.print("Enter number of years: ");
+		int years = sc.nextInt();
+
+		double futureInvestmentValue = amount * Math.pow(1 + monthlyInterestRate, years * 12);
+
+		System.out.println("Accumulated value is $" + futureInvestmentValue);
+    }
+}

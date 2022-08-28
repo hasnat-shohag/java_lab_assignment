2cf9da32fc39957c14034380f3d3f46658dc1748
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-08-29 00:35:38 +0600

    7 no added, refined code

--- Assignment 2/assignment_2_7.java
+++ Assignment 2/assignment_2_7.java
@@ -1,85 +1,34 @@
 import java.util.Scanner;
-class operator{
-    public void checkOperator(char ch){
-        if(ch == '1'){
-            System.out.println("Citycell");
-        }
-        else if(ch == '5'){
-            System.out.println("Teletalk");
-        }
-        else if(ch == '7' || ch == '3'){
-            System.out.println("Grameenphone");
-        }
-        else if(ch == '6' || ch =='8'){
-            System.out.println("Robi");
-        }
-        else{
-            System.out.println("Banglalink");
-        }
-    }
-}
+
 public class assignment_2_7 {
     public static void main(String[] args) {
+        System.out.println("Please enter the phone number");
         Scanner sc = new Scanner(System.in);
-        operator myObj = new operator();
-        String number = sc.nextLine();
-        String s = "13456789";
-        String helper = "+8801";
-
-        int len = number.length();
-        if (len == 14 || len == 11) {
-            if (len == 14) {
-                int cnt = 0;
-                for (int i = 0; i < 5; i++) {
-                    if (number.charAt(i) == helper.charAt(i)) {
-                        cnt++;
-                    }
-                }
-                if (cnt == 5) {
-                    int f = 0;
-                    for (int i = 0; i < 8; i++) {
-                        if (number.charAt(5) == s.charAt(i)) {
-                            f = 1;
-                            myObj.checkOperator(number.charAt(5));
-                            System.out.println("A bangladeshi number");
-                            break;                    }
-                    }
-                    if (f == 0){
-                        System.out.println("It is not a bangladeshi number");
-                    }
-                } 
-                else {
-                    System.out.println("It is not a bangladeshi number");
-                }
-            } 
-            else {
-                int cnt = 0;
-                for (int i = 0; i < 2; i++) {
-                    if (number.charAt(i) == helper.charAt(i+3)) {
-                        cnt++;
-                    }
-                }
-                if (cnt == 2) {
-                    int f = 0;
-                    for (int i = 0; i < 8; i++) {
-                        if (number.charAt(2) == s.charAt(i)) {
-                            f = 1;
-                            myObj.checkOperator(number.charAt(2));
-                            System.out.println("A bangladeshi number");
-                            break;
-                        }
-                    }
-                    if (f == 0){
-                        System.out.println("It is not a bangladeshi number");
-                    }
-                } 
-                else {
-                    System.out.println("It is not a bangladeshi number");
-                }
+        String PhoneNumber = sc.next();
+        if(PhoneNumber.charAt(0) == '+'){
+            if(PhoneNumber.charAt(1) != '8' || PhoneNumber.charAt(2) != '8'){
+                System.out.println("Given number is not a valid Bangladeshi Number");
+                return;
             }
-        } 
-        else {
-            System.out.println("It is not a bangladeshi number");
+            PhoneNumber = PhoneNumber.substring(3,PhoneNumber.length());
+        }
+        if(PhoneNumber.length() != 11){
+            System.out.println("Given number is not a valid Bangladeshi Number");
+            return;
+        }
+        String operatorCode = PhoneNumber.substring(1,3);
+        if(operatorCode.equals("11")){
+            System.out.println("The operator name is Citycell");
+        }else if(operatorCode.equals("13") || operatorCode.equals("17")){
+            System.out.println("The operator name is Grameenphone");
+        }else if(operatorCode.equals("14") || operatorCode.equals("19")){
+            System.out.println("The operator name is Banglalink");
+        }else if(operatorCode.equals("15")){
+            System.out.println("The operator name is TeleTalk");
+        }else if(operatorCode.equals("16") || operatorCode.equals("18")){
+            System.out.println("The operator name is Robi");
+        }else{
+            System.out.println("Given number is not a valid Bangladeshi Number");
         }
     }
 }

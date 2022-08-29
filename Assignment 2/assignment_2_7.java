da8215708db897f44815d4e58493a5f487ff334f
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-08-29 09:50:23 +0600

    bug fixed

--- Assignment 2/assignment_2_7.java
+++ Assignment 2/assignment_2_7.java
@@ -5,29 +5,35 @@ public class assignment_2_7 {
         System.out.println("Please enter the phone number");
         Scanner sc = new Scanner(System.in);
         String PhoneNumber = sc.next();
-        if(PhoneNumber.charAt(0) == '+'){
-            if(PhoneNumber.charAt(1) != '8' || PhoneNumber.charAt(2) != '8'){
+        if (PhoneNumber.charAt(0) == '+') {
+            if (PhoneNumber.charAt(1) != '8' || PhoneNumber.charAt(2) != '8') {
                 System.out.println("Given number is not a valid Bangladeshi Number");
                 return;
             }
-            PhoneNumber = PhoneNumber.substring(3,PhoneNumber.length());
+            PhoneNumber = PhoneNumber.substring(3, PhoneNumber.length());
         }
-        if(PhoneNumber.length() != 11){
+        if (PhoneNumber.length() != 11) {
             System.out.println("Given number is not a valid Bangladeshi Number");
             return;
         }
-        String operatorCode = PhoneNumber.substring(1,3);
-        if(operatorCode.equals("11")){
+        for (int i = 0; i < PhoneNumber.length(); i++) {
+            if (PhoneNumber.charAt(i) < '0' || PhoneNumber.charAt(i) > '9') {
+                System.out.println("Given number is not a valid Bangladeshi Number");
+                return;
+            }
+        }
+        String operatorCode = PhoneNumber.substring(1, 3);
+        if (operatorCode.equals("11")) {
             System.out.println("The operator name is Citycell");
-        }else if(operatorCode.equals("13") || operatorCode.equals("17")){
+        } else if (operatorCode.equals("13") || operatorCode.equals("17")) {
             System.out.println("The operator name is Grameenphone");
-        }else if(operatorCode.equals("14") || operatorCode.equals("19")){
+        } else if (operatorCode.equals("14") || operatorCode.equals("19")) {
             System.out.println("The operator name is Banglalink");
-        }else if(operatorCode.equals("15")){
+        } else if (operatorCode.equals("15")) {
             System.out.println("The operator name is TeleTalk");
-        }else if(operatorCode.equals("16") || operatorCode.equals("18")){
+        } else if (operatorCode.equals("16") || operatorCode.equals("18")) {
             System.out.println("The operator name is Robi");
-        }else{
+        } else {
             System.out.println("Given number is not a valid Bangladeshi Number");
         }
     }

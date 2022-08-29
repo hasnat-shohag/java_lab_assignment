f1b7339324e7d32743fc81c535f84f64cf312ae9
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-08-29 09:39:36 +0600

    palindrome number range added

--- Assignment 2/assignment_2_4.java
+++ Assignment 2/assignment_2_4.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
-class palindromes{
-    public boolean checkPalindromes(int n){
+
+class palindromes {
+    public boolean checkPalindromes(int n) {
         String s = "";
 
-        while(n > 0){
+        while (n > 0) {
             int p = n % 10;
-            s += (char)p;
+            s += (char) p;
             n /= 10;
         }
         int len = s.length();
-        for(int i = 0,j=len-1; i<len/2; i++,j--){
-            if(s.charAt(i) != s.charAt(j)){
+        for (int i = 0, j = len - 1; i < len / 2; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
         }
         return true;
     }
 }
+
 public class assignment_2_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -24,10 +26,22 @@ public class assignment_2_4 {
         n = sc.nextInt();
         palindromes myObj = new palindromes();
         boolean flag = myObj.checkPalindromes(n);
-        if (flag){
+        if (flag) {
             System.out.println(n + " is Palindrome");
-        }else{
+        } else {
             System.out.println(n + " is not Palindrome");
+        }
+
+        System.out.println("\nEnter the range :");
+        int low,high;
+        low = sc.nextInt();
+        high = sc.nextInt();
+
+        System.out.println("Palindrome numbers in this range : ");
+        for (int i = low; i < high; i++) {
+            if (myObj.checkPalindromes(i)) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
---
+++ Assignment 2/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+ // for(int i = 1; i<9999999; i++){
+        //     if(myObj.checkPalindromes(i)){
+        //         System.out.println(i + " is palindrome");
+        //     }
+        // }

6e73b6eefa5814577581e1fcf61d8943d4de2691
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-08-29 10:08:08 +0600

    simplified name

--- Assignment 1/_5_full_name_to_first_middle_and_last_name.java
+++ Assignment 1/_5_full_name_to_first_middle_and_last_name.java
@@ -7,52 +7,69 @@ public class _5_full_name_to_first_middle_and_last_name {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Full Name: ");
         String s = sc.nextLine();
-        int len = s.length();
-        String fn="",mn="",ln="";
-        int space=0;
-        int nos = 0;
-        // count the parts of the name
-        for(int i = 0; i<len; i++){
-            if(s.charAt(i) == ' ')nos++;
-        }
 
-        if(nos == 2){
-            for(int i = 0; i<len; i++){
-                if(s.charAt(i)==' '){
-                    space = i;
-                    break;
-                }
-                fn += s.charAt(i);
-            }
-            for(int i = space+1; i<len; i++){
-                if(s.charAt(i)==' '){
-                    space = i;
-                    break;
-                }
-                mn += s.charAt(i);
-            }
-            for(int i = space+1; i<len; i++){
-                ln += s.charAt(i);
-            }
-            System.out.println("First Name: "+fn);
-            System.out.println("Middle Name: "+mn);
-            System.out.println("Last Name: "+ln);
+        String[] res = new String[5];
+        for(int i = 0; i<5; i++){
+            res[i] = "";
         }
-        else{
-            for(int i = 0; i<len; i++){
-                if(s.charAt(i)==' '){
-                    space = i;
-                    break;
-                }
-                fn += s.charAt(i);
+        int idx = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == ' '){
+                idx++;
+                continue;
             }
+            res[idx] += s.charAt(i);
+        }
+        System.out.println("First Name: "+res[0]+"\nMiddle Name: "+res[1]+"\nLast Name: "+ res[2]);
+
+
+
+        // int len = s.length();
+        // String fn="",mn="",ln="";
+        // int space=0;
+        // int nos = 0;
+        // count the parts of the name
+        // for(int i = 0; i<len; i++){
+        //     if(s.charAt(i) == ' ')nos++;
+        // }
+        
+    //     if(nos == 2){
+    //         for(int i = 0; i<len; i++){
+    //             if(s.charAt(i)==' '){
+    //                 space = i;
+    //                 break;
+    //             }
+    //             fn += s.charAt(i);
+    //         }
+    //         for(int i = space+1; i<len; i++){
+    //             if(s.charAt(i)==' '){
+    //                 space = i;
+    //                 break;
+    //             }
+    //             mn += s.charAt(i);
+    //         }
+    //         for(int i = space+1; i<len; i++){
+    //             ln += s.charAt(i);
+    //         }
+    //         System.out.println("First Name: "+fn);
+    //         System.out.println("Middle Name: "+mn);
+    //         System.out.println("Last Name: "+ln);
+    //     }
+    //     else{
+    //         for(int i = 0; i<len; i++){
+    //             if(s.charAt(i)==' '){
+    //                 space = i;
+    //                 break;
+    //             }
+    //             fn += s.charAt(i);
+    //         }
             
-            for(int i = space+1; i<len; i++){
-                ln += s.charAt(i);
-            }
-            System.out.println("First Name: "+fn);
-            System.out.println("Middle Name: "+mn);
-            System.out.println("Last Name: "+ln);
-        }
+    //         for(int i = space+1; i<len; i++){
+    //             ln += s.charAt(i);
+    //         }
+    //         System.out.println("First Name: "+fn);
+    //         System.out.println("Middle Name: "+mn);
+    //         System.out.println("Last Name: "+ln);
+    //     }
     }
 }

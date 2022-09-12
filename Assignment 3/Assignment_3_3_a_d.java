28767e544487534b08b888c6932c5d5a08770366
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-09-12 17:13:54 +0600

    3_3_a_d

---
+++ Assignment 3/Assignment_3_3_a_d.java
@@ -0,0 +1,26 @@
+// Grid Layout 
+import java.awt.*;
+import javax.swing.*;
+
+class GridLayoutImp{
+    GridLayoutImp(){
+        JFrame fm = new JFrame();
+        fm.setTitle("Grid Layout");
+        // GridLayout(row, columns, horizontal gap, vertical gap)
+        fm.setLayout(new GridLayout(1,3,10,0));
+
+        JLabel l = new JLabel("Label");  
+        JTextField tf =new JTextField("Text");  
+        JButton b = new JButton("Send");
+        
+        fm.add(l);fm.add(tf);fm.add(b);
+
+        fm.setSize(300,300);
+        fm.setVisible(true);
+    }
+}
+public class Assignment_3_3_a_d {
+    public static void main(String[] args) {
+        new GridLayoutImp();
+    }
+}

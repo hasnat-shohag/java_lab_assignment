7505d1cc5f8a0509ffc943dc64d125a6ef3e44e8
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-09-12 17:55:43 +0600

    3_3_b_c

--- Assignment 3/Assignment_3_3_b_c.java
+++ Assignment 3/Assignment_3_3_b_c.java
@@ -1,3 +1,28 @@
-public class Assignment_3_3_b_a {
-    
+// Using Jframe Class 
+// Border Layout
+import java.awt.*;
+import javax.swing.*;
+
+class Border{
+    Border(){
+        Frame fm = new JFrame();
+        fm.setLayout(new BorderLayout(10,10));
+
+        Label l = new Label("Label");  
+        TextField tf = new TextField("Text");  
+        Button b = new Button("Send");
+
+        fm.add(l,BorderLayout.WEST); 
+        fm.add(tf,BorderLayout.CENTER);  
+        fm.add(b,BorderLayout.EAST);
+        
+        fm.setSize(300,300);
+        fm.setVisible(true);
+    }
+}
+
+public class Assignment_3_3_b_c {
+    public static void main(String[] args) {
+        new Border();
+    }
 }

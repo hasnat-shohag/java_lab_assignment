37fe89ccccdf314fc044f78fc5dfc9075840d8b1
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-09-12 13:15:41 +0600

    2 no done

---
+++ Assignment 3/Assignment_3_2.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Assignment_3_2
+{
+  public static void main ( String[] args )
+  {
+
+    JFrame master = new JFrame("Click to Close Everything");
+    JFrame temp = new JFrame("Click to Close Just This");
+
+    temp.setVisible(true);
+    temp.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE  );
+    temp.setSize( 400, 300 );
+
+    master.setVisible(true);
+    master.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    master.setSize( 500, 400 );
+    
+
+  }
+}

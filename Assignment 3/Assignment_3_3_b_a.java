5cbd3151419f4950f33bce164198809429e48251
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-09-12 17:24:20 +0600

    3_3_b_a

---
+++ Assignment 3/Assignment_3_3_b_a.java
@@ -0,0 +1,27 @@
+// Using Jframe Class 
+// Default Layout
+import java.awt.*;
+
+public class Assignment_3_3_b_a {
+    public static void main(String[] args) {
+        Frame fm = new Frame();
+        fm.setTitle("Default Layout");
+
+        Label l=new Label("Label");  
+        // setBounds(x axis,y axis, widht, height)
+        l.setBounds(100,100, 40,50);
+        fm.add(l);    
+
+        TextField tf =new TextField("Text");  
+        tf.setBounds(145,100,150,50);   
+        fm.add(tf);  
+
+        Button b = new Button("Send");
+        b.setBounds(300,100,100,50);
+        fm.add(b);
+
+        fm.setSize(600,600);
+        fm.setLayout(null);
+        fm.setVisible(true);
+    }
+}
---
+++ Assignment 3/Assignment_3_3_b_b.java
@@ -0,0 +1,3 @@
+public class Assignment_3_3_b_a {
+    
+}

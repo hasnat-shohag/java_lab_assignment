f8f7c2a4431be807902fad849ab006b43a7f3694
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-09-12 17:52:23 +0600

    3_3_b_b

--- Assignment 3/Assignment_3_3_b_b.java
+++ Assignment 3/Assignment_3_3_b_b.java
@@ -1,21 +1,22 @@
 // Flow Layout 
 import java.awt.*;
 import javax.swing.*;
+
 class FlowLayoutImp{
     FlowLayoutImp(){
-        JFrame fm = new JFrame();
+        Frame fm = new Frame();
         fm.setTitle("FlowLayout Design");
         
         //Setting up Flow Layout
         fm.setLayout(new FlowLayout(FlowLayout.CENTER));
         
-        JLabel l = new JLabel("Label");  
+        Label l = new Label("Label");  
         fm.add(l);    
         
-        JTextField tf =new JTextField("Text");  
+        TextField tf = new TextField("Text");  
         fm.add(tf); 
 
-        JButton b = new JButton("Send");
+        Button b = new Button("Send");
         fm.add(b);
 
         fm.setSize(400,400);

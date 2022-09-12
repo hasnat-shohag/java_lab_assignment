ef6e1df3817dbd6144846cfb298a6459bfae3533
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-09-12 17:25:39 +0600

    3_3_a_a

--- Assignment 3/Assignment_3_3_a_a.java
+++ Assignment 3/Assignment_3_3_a_a.java
@@ -5,14 +5,19 @@ import javax.swing.*;
 public class Assignment_3_3_a_a {
     public static void main(String[] args) {
         JFrame fm = new JFrame();
+
+        // setBounds(x axis,y axis, widht, height)
         JLabel l=new JLabel("Label");  
         l.setBounds(100,100, 40,50);
-        fm.add(l);    
+
         JTextField tf =new JTextField("Text");  
-        tf.setBounds(145,100,150,50);   
-        fm.add(tf);  
-        JButton b = new JButton("Click Me");
+        tf.setBounds(145,100,150,50);
+
+        JButton b = new JButton("Send");
         b.setBounds(300,100,100,50);
+
+        fm.add(l);    
+        fm.add(tf);  
         fm.add(b);
         fm.setSize(600,600);
         fm.setLayout(null);
